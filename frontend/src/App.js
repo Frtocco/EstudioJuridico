49cb713126dc2d93e1a7864bbab551bf09464253
Francisco <frtocco@uade.edu.ar>
2025-04-10 12:49:26 -0300

    Se definio todo por componentes y agrego la cita

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,18 @@ import react from 'react';
 import Navbar from './components/Navbar.js'
 import AreaPracticaCard from './components/AreaPractica.js'
 import AboutUs from './components/AboutUs.js';
-import justice from './images/justice.jpg';
+import MainLanding from './components/MainLanding.js';
+import Quote from './components/quote.js';
+
 
 
 function App() {
   return (
     <div className="flex flex-col items-center">
       <Navbar />
-      <div id="inicio" className="relative h-[40rem] w-full">
-        <div 
-          className="absolute inset-0 bg-cover"
-          style={{ backgroundImage: `url(${justice})` }}
-        ></div>
-        <div className="absolute inset-0 bg-black bg-opacity-30"></div>
-        <div className="relative z-10 flex justify-center pt-20">
-          <div className='flex flex-col'>
-            <h1 className="text-white text-5xl font-bold"> Estudio jurídico Tocco y Asociados</h1>
-            <p className='text-white text-2xl text-center'> Con mas de 30 años de experiencia ayudando a la comunidad </p>
-          </div>
-        </div>
+      <MainLanding/>
+      <div>
+        <Quote/>
       </div>
       <div id="nosotros">
         <AboutUs/>
